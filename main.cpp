8e4588c9c20a820edf6c39cb8e1cabd021bd7ce0
elena <haleeva-elena@mail.ru>
2023-04-07 18:52:42 +0300

    Add GetDataFromFile()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+string GetDataFromFile(string path);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    string path = "input.dat";
+    cout << GetDataFromFile(path) << endl;
+
     return 0;
+}
+
+string GetDataFromFile(string path) {
+    string line = "";
+
+    ifstream file;
+    file.open(path);
+
+    if (!file.is_open()) {
+        return line;
+    }
+
+    getline(file, line);
+
+    file.close();
+
+    return line;
 }
