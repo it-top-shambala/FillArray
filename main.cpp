ec441199dd6985d4e4a96d7e71079c8987b8fa46
elena <haleeva-elena@mail.ru>
2023-04-07 19:03:56 +0300

    Add validateData(), calcSizeArray()

--- main.cpp
+++ main.cpp
@@ -6,10 +6,21 @@ using namespace std;
 
 string GetDataFromFile(string path);
 
+bool validateData(string data);
+
+int calcSizeArray(string data);
+
 int main() {
 
     string path = "input.dat";
-    cout << GetDataFromFile(path) << endl;
+
+    string data = GetDataFromFile(path);
+    cout << data << endl;
+
+    cout << validateData(data) << endl;
+
+    int size = calcSizeArray(data);
+    cout << size << endl;
 
     return 0;
 }
@@ -29,4 +40,20 @@ string GetDataFromFile(string path) {
     file.close();
 
     return line;
+}
+
+bool validateData(string data) {
+    return !data.empty();
+}
+
+int calcSizeArray(string data) {
+    int size = 1;
+
+    for (int i = 0; i < data.length(); ++i) {
+        if (data[i] == ' ') {
+            size++;
+        }
+    }
+
+    return size;
 }
